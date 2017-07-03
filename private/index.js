980d750ad8617b6c1a08074bc3795bef7d26e231
Your Name <you@example.com>
2017-07-03 16:07:38 -0700

    fixing

--- private/index.js
+++ private/index.js
@@ -17,6 +17,13 @@ const inventoryCtrl = require("./inventoryCtrl.js");
 
 app.get("/api/shelf/:id", inventoryCtrl.getShelf);
 app.get("/api/bin/:id", inventoryCtrl.getBin);
+app.put("/api/bin/:id", inventoryCtrl.)
+
+// GET - /api/shelf/:id - Return all inventory objects for bins 1-5 in array format: ( [ {}, null, null, {}, {} ] )
+// GET - /api/bin/:id - Return inventory object located at id. id will look like: "A3", we can split this string and the first element will always be the shelf ID and the second element will always be the bin ID
+// PUT - /api/bin/:id - Return updated inventory object. ID is same concept as above. I will send an object with any of the following properties: name, image, price
+// DELETE - /api/bin/:id - Remove inventory from bin.
+// POST - /api/bin/:id - Create inventory object. I will send an object with the following properties: name, shelf, bin, image, and price
 
 const port = 3000;
 
