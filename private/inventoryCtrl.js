7b0e46d59b49205ad1867bb82c580de6c3521cd2
James Lemire <webdev.jameslemire@gmail.com>
2017-07-03 16:23:09 -0600

    JL master

--- private/inventoryCtrl.js
+++ private/inventoryCtrl.js
@@ -2,7 +2,18 @@ module.exports = {
   getShelf: (req, res, next) => {
     const db = req.app.get('db');
     db.get_shelf(req.params.id).then(inventory => {
-      res.status(200).send(inventory);
+      let maxBins = 5;
+      let bins = [];
+      
+      for( var i = 0; i < maxBins; i++ ) {
+        if ( inventory[i] ) {
+          bins[ inventory[i].bin - 1 ] = inventory[i];
+        } else if ( !bins[i] ) {
+          bins[i] = null;
+        }
+      };
+
+      res.status(200).send( bins );
     }).catch(function(err) {
       console.log(err);
     })
